2b809f379783d15900307b56bc02a90913511470
reverser <fg@put.as>
2014-11-10 23:36:11 +0000

    Add the missing config.h file.

---
+++ MPRESS Dumper/config.h
@@ -0,0 +1,58 @@
+/*
+ * ._____.___ ._______ .______  ._______.________.________
+ * :         |: ____  |: __   \ : .____/|    ___/|    ___/
+ * |   \  /  ||    :  ||  \____|| : _/\ |___    \|___    \
+ * |   |\/   ||   |___||   :  \ |   /  \|       /|       /
+ * |___| |   ||___|    |   |___\|_.: __/|__:___/ |__:___/
+ * |___|         |___|       :/      :        :
+ *
+ * .______  .____     ._____.___ ._______ ._______.______
+ * :_ _   \ |    |___ :         |: ____  |: .____/: __   \
+ * |   |   ||    |   ||   \  /  ||    :  || : _/\ |  \____|
+ * | . |   ||    :   ||   |\/   ||   |___||   /  \|   :  \
+ * |. ____/ |        ||___| |   ||___|    |_.: __/|   |___\
+ * :/      |. _____/       |___|            :/   |___|
+ * :        :/
+ *          :
+ *
+ * MPRESS Packer dumper
+ *
+ * Created by Pedro Vilaca on 26/01/14.
+ * Copyright (c) 2014 Pedro Vilaca. All rights reserved.
+ * http://reverse.put.as - reverser@put.as
+ *
+ * config.h
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef MPRESS_Dumper_config_h
+#define MPRESS_Dumper_config_h
+
+/* option to dump to disk the different unpacking stages
+ * good for analysis without manual unpacking
+ */
+#define DUMP_STAGES 1
+
+#endif
